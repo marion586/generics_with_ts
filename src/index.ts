fe8b7a615bd921e7379ba7742bec1baa64ca56f7
marion586 <marionmenye@gmail.com>
2024-11-20 15:34:09 +0300

    use generics function

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,18 @@ const message: string = "Hello world by Ahsan!";
 console.log(message);
 
 export const getMessage = (): string => message;
+
+function identity<T>(arg: T): T {
+  return arg;
+}
+
+let output = identity<string>("hello");
+console.log(output);
+
+function identityDouble<T, G>(arg: T, arg1: G): G {
+  console.log(arg);
+  return arg1;
+}
+
+let output1 = identityDouble<string, number>("hello", 2);
+console.log(output1);
